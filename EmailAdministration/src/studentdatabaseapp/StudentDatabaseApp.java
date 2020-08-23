d5b9da12f42b8e54171d4f74b18a3c7de730679d
tichuot <tichuot287@gmail.com>
2020-08-23 16:10:55 +0700

    Student management project added

--- EmailAdministration/src/studentdatabaseapp/StudentDatabaseApp.java
+++ EmailAdministration/src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,4 +1,24 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
+    public static void main(String[] args){
+        // input number of students
+        System.out.println("Enter number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numberOfStudents = in.nextInt();
+        Student[] students = new Student[numberOfStudents];
+
+        //Create a number of students
+        for (int n =0; n < numberOfStudents; n++){
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+        }
+
+        for (int n = 0; n < numberOfStudents; n++){
+            System.out.print(students[n].toString());
+        }
+    }
 }
